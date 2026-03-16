fa01460eca52f7cc199ef93aa191d44bf9d83c6a
ivySageTafe <ivy.sage@student.tafesa.edu.au>
2026-03-16 12:54:06 +1030

    Commit the branch feat_wrap1.1

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -31,10 +31,12 @@ namespace ProductApps
         {
             try
             {
+                
                 cProduct = new Product(Convert.ToDecimal(priceTextBox.Text), Convert.ToInt16(quantityTextBox.Text));
                 cProduct.calTotalPayment();
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
                 totalChargeTextBlock.Text = Convert.ToString(cProduct.TotalPayment + 25);
+                wrappingTextBlock.Text = Convert.ToString(cProduct.TotalPayment + 30);
             }
             catch (FormatException)
             {
